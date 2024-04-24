4394ed21c8feef6e59e35376a035b4205aa30806
amir4774 <amirabbassi1382@gmail.com>
2024-04-24 16:54:00 +0330

    add Confetti and add react-use

--- src/Pages/Result.tsx
+++ src/Pages/Result.tsx
@@ -1,5 +1,7 @@
 import { Box } from "@mui/material";
 import { Navigate } from "react-router-dom";
+import Confetti from "react-confetti";
+import useWindowSize from "react-use/lib/useWindowSize";
 import useStore from "../Zustand/Store";
 import Chart from "../Components/Result/Chart";
 import ResultTitle from "../Components/Result/ResultTitle";
@@ -7,6 +9,7 @@ import PlayAgain from "../Components/Result/PlayAgain";
 
 const Result = () => {
   const { result } = useStore();
+  const { height, width } = useWindowSize();
 
   if (result.correct === 0 && result.incorrect === 0) {
     return <Navigate to="/exam" />;
@@ -14,7 +17,14 @@ const Result = () => {
 
   return (
     <div>
-      <Box width="90%" mx="auto">
+      <Box width="95%" mx="auto">
+        <Confetti
+          width={width}
+          height={height}
+          numberOfPieces={850}
+          confettiSource={{ x: 0, y: 0, w: width, h: 0 }}
+          recycle={false}
+        />
         <ResultTitle />
         <Chart />
         <PlayAgain />
