01da4be4876838cb8a0143b0421b8c267cfcd720
amir4774 <amirabbassi1382@gmail.com>
2024-04-24 17:04:53 +0330

    for vercel

--- src/Zustand/Store.ts
+++ src/Zustand/Store.ts
@@ -1,5 +1,27 @@
 import { create } from "zustand";
-import { Store, ExamParamsType } from "./Interfaces";
+// import { Store, ExamParamsType } from "./Interfaces";
+export interface ExamParamsType {
+  category: string;
+  difficulty: string;
+  limit: number;
+}
+
+export interface ResultType {
+  correct: number;
+  incorrect: number;
+}
+
+export interface Store {
+  mode: string;
+  userName: string;
+  examParams: ExamParamsType;
+  result: ResultType;
+  changeMode: () => void;
+  changeUserName: (name: string) => void;
+  changeExamParams: (params: ExamParamsType) => void;
+  changeResult: (status: "correct" | "incorrect") => void;
+  resetResult: () => void;
+}
 
 const useStore = create<Store>((set, get) => ({
   mode: sessionStorage.getItem("mode") || "dark",
