b86348c4d8fbe610b676d0381c43612afed6f1ca
amir4774 <amirabbassi1382@gmail.com>
2025-02-10 22:32:03 +0330

    feat: add captcha

--- src/Components/Login/LoginForm.tsx
+++ src/Components/Login/LoginForm.tsx
@@ -1,131 +1,144 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { toast } from "react-toastify";
+import {useState} from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import {Link, useNavigate} from "react-router-dom";
+import {useForm} from "react-hook-form";
+import {toast} from "react-toastify";
 import {
-  Box,
-  Button,
-  FormControl,
-  FormGroup,
-  FormHelperText,
-  IconButton,
-  InputAdornment,
-  TextField,
-  Typography,
+    Box,
+    Button,
+    FormControl,
+    FormGroup,
+    FormHelperText,
+    IconButton,
+    InputAdornment,
+    TextField,
+    Typography,
 } from "@mui/material";
-import { AxiosResponse, isAxiosError } from "axios";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {AxiosResponse, isAxiosError} from "axios";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
 import QuizGradTitle from "../QuizGradTitle";
 import useStore from "../../Zustand/Store";
 import useShowPassword from "../../Hooks/useShowPassword";
 import InternalApi from "../../Services/InternalApi";
-import { LoginData, LoginResponse } from "../SignUp_Login/Interfaces";
+import {LoginData, LoginResponse} from "../SignUp_Login/Interfaces";
 import "../SignUp_Login/SignUp_Login_Style.css";
 import useGlobalTranslation from "../../Hooks/useGlobalTranslation";
+
 const LoginForm = () => {
-  const showPassword = useShowPassword(false);
-  const [loading, setLoading] = useState(false);
-  const { changeUserName, changeToken } = useStore();
-  const navigate = useNavigate();
-  const { t } = useGlobalTranslation();
+    const [loading, setLoading] = useState(false);
+    const [captchaValue, setCaptchaValue] = useState<string | null>(null);
+    const showPassword = useShowPassword(false);
+    const {changeUserName, changeToken} = useStore();
+    const navigate = useNavigate();
+    const {t} = useGlobalTranslation();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginData>();
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm<LoginData>();
 
-  const onSubmit = async (data: LoginData) => {
-    try {
-      setLoading(true);
+    const onSubmit = async (data: LoginData) => {
+        if (!captchaValue) return
 
-      const res: AxiosResponse<LoginResponse> = await InternalApi().post(
-        "login/",
-        {
-          username: data.name,
-          password: data.password,
+        try {
+            setLoading(true);
+
+            const res: AxiosResponse<LoginResponse> = await InternalApi().post(
+                "login/",
+                {
+                    username: data.name,
+                    password: data.password,
+                }
+            );
+
+            toast.success(t("Welcome back!") + ` ${data.name}`);
+            changeUserName(data.name);
+
+            localStorage.setItem("token", res.data.access);
+            changeToken(res.data.access);
+
+            navigate("/");
+        } catch (error: unknown) {
+            if (isAxiosError(error)) {
+                if (error.response?.status === 401) {
+                    toast.error(t("Wrong username or password"));
+                } else {
+                    toast.error(t("Something went wrong"));
+                }
+            } else {
+                toast.error(t("An unexpected error occurred"));
+            }
+        } finally {
+            setLoading(false);
         }
-      );
+    };
 
-      toast.success(t("Welcome back!") + ` ${data.name}`);
-      changeUserName(data.name);
+    return (
+        <Box color="text.primary" textAlign="center" px={5} py={3} width="100%">
+            <QuizGradTitle/>
 
-      localStorage.setItem("token", res.data.access);
-      changeToken(res.data.access);
+            <Typography color="text.secondary">
+                {t("Welcome back!")} <br/> {t("Please Login to your account.")}
+            </Typography>
 
-      navigate("/");
-    } catch (error: unknown) {
-      if (isAxiosError(error)) {
-        if (error.response?.status === 401) {
-          toast.error(t("Wrong username or password"));
-        } else {
-          toast.error(t("Something went wrong"));
-        }
-      } else {
-        toast.error(t("An unexpected error occurred"));
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
+            <Link to="/sign-up" style={{display: "inline-block", marginTop: "8px"}}>
+                <Typography color="text.primary" fontWeight="bold">
+                    {t("don't have an account?")}
+                </Typography>
+            </Link>
 
-  return (
-    <Box color="text.primary" textAlign="center" px={5} width="100%">
-      <QuizGradTitle />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <FormGroup sx={{gap: "20px", mt: 4}}>
+                    <FormControl fullWidth>
+                        <TextField
+                            label={t("Name")}
+                            {...register("name", {required: t("Name is required")})}
+                            name="name"
+                        />
+                        <FormHelperText error>{errors.name?.message}</FormHelperText>
+                    </FormControl>
 
-      <Typography color="text.secondary">
-        {t("Welcome back!")} <br /> {t("Please Login to your account.")}
-      </Typography>
+                    <FormControl fullWidth>
+                        <TextField
+                            {...register("password", {
+                                required: t("Password is required"),
+                                minLength: {
+                                    value: 8,
+                                    message: t("Password must be at least 8 characters"),
+                                },
+                            })}
+                            name="password"
+                            label={t("Password")}
+                            type={showPassword.value ? "text" : "password"}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton onClick={showPassword.OnClick}>
+                                            {showPassword.value ? <VisibilityOff/> : <Visibility/>}
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                        <FormHelperText error>{errors.password?.message}</FormHelperText>
+                    </FormControl>
 
-      <Link to="/sign-up" style={{ display: "inline-block", marginTop: "8px" }}>
-        <Typography color="text.primary" fontWeight="bold">
-          {t("don't have an account?")}
-        </Typography>
-      </Link>
+                    <FormControl fullWidth>
+                        <ReCAPTCHA
+                            sitekey="6LcCH9EqAAAAAIjMRsfBKIEcAY8G7jNN-pr6gLLb"
+                            onChange={(value: string | null) => setCaptchaValue(value)}
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FormGroup sx={{ gap: "20px", mt: 4 }}>
-          <FormControl fullWidth>
-            <TextField
-              label={t("Name")}
-              {...register("name", { required: t("Name is required") })}
-              name="name"
-            />
-            <FormHelperText error>{errors.name?.message}</FormHelperText>
-          </FormControl>
+                        />
+                    </FormControl>
 
-          <FormControl fullWidth>
-            <TextField
-              {...register("password", {
-                required: t("Password is required"),
-                minLength: {
-                  value: 8,
-                  message: t("Password must be at least 8 characters"),
-                },
-              })}
-              name="password"
-              label={t("Password")}
-              type={showPassword.value ? "text" : "password"}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton onClick={showPassword.OnClick}>
-                      {showPassword.value ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <FormHelperText error>{errors.password?.message}</FormHelperText>
-          </FormControl>
-
-          <Button variant="useful" type="submit" disabled={loading}>
-            {loading ? t("Loading") : t("Login")}
-          </Button>
-        </FormGroup>
-      </form>
-    </Box>
-  );
+                    <Button variant="useful" type="submit" disabled={loading}>
+                        {loading ? t("Loading") : t("Login")}
+                    </Button>
+                </FormGroup>
+            </form>
+        </Box>
+    );
 };
 
 export default LoginForm;
--- src/Components/SignUp/SignUpForm.tsx
+++ src/Components/SignUp/SignUpForm.tsx
@@ -1,149 +1,160 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { toast } from "react-toastify";
+import {useState} from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import {useNavigate} from "react-router-dom";
+import {useForm} from "react-hook-form";
+import {toast} from "react-toastify";
 import {
-  Box,
-  Button,
-  FormControl,
-  FormGroup,
-  FormHelperText,
-  IconButton,
-  InputAdornment,
-  TextField,
-  Typography,
+    Box,
+    Button,
+    FormControl,
+    FormGroup,
+    FormHelperText,
+    IconButton,
+    InputAdornment,
+    TextField,
+    Typography,
 } from "@mui/material";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
 import QuizGradTitle from "../QuizGradTitle";
 import useShowPassword from "../../Hooks/useShowPassword";
-import { SignUpData } from "../SignUp_Login/Interfaces";
+import {SignUpData} from "../SignUp_Login/Interfaces";
 import "../SignUp_Login/SignUp_Login_Style.css";
 import InternalApi from "../../Services/InternalApi";
 import useGlobalTranslation from "../../Hooks/useGlobalTranslation";
-import { AxiosError } from "axios";
+import {AxiosError} from "axios";
 
 const SignUpForm = () => {
-  const [loading, setLoading] = useState(false);
-  const showPassword = useShowPassword(false);
-  const showConfirmPassword = useShowPassword(false);
-  const navigate = useNavigate();
-  const { t } = useGlobalTranslation();
+    const [loading, setLoading] = useState(false);
+    const [captchaValue, setCaptchaValue] = useState<string | null>(null);
+    const showPassword = useShowPassword(false);
+    const showConfirmPassword = useShowPassword(false);
+    const navigate = useNavigate();
+    const {t} = useGlobalTranslation();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<SignUpData>();
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm<SignUpData>();
 
-  const onSubmit = async (data: SignUpData) => {
-    try {
-      setLoading(true);
-      if (data.password !== data.confirmPassword) {
-        toast.error(t("Passwords do not match"));
-        return;
-      }
+    const onSubmit = async (data: SignUpData) => {
+        if (!captchaValue) return
 
-      await InternalApi().post("register/", {
-        username: data.name,
-        password: data.password,
-      });
+        try {
+            setLoading(true);
+            if (data.password !== data.confirmPassword) {
+                toast.error(t("Passwords do not match"));
+                return;
+            }
 
-      toast.success(t("Account created successfully. Please log in."));
-      navigate("/login");
-    } catch (err: AxiosError | any) {
-      if (err.response.status === 400) {
-        if (err.response.data.username) {
-          toast.error(t("Username already exists"));
+            await InternalApi().post("register/", {
+                username: data.name,
+                password: data.password,
+            });
+
+            toast.success(t("Account created successfully. Please log in."));
+            navigate("/login");
+        } catch (err: AxiosError | any) {
+            if (err.response.status === 400) {
+                if (err.response.data.username) {
+                    toast.error(t("Username already exists"));
+                }
+            }
+            console.log(err);
+        } finally {
+            setLoading(false);
         }
-      }
-      console.log(err);
-    } finally {
-      setLoading(false);
-    }
-  };
+    };
 
-  return (
-    <Box color="text.primary" textAlign="center" px={5} width="100%">
-      <QuizGradTitle />
+    return (
+        <Box color="text.primary" textAlign="center" px={5} py={3} width="100%">
+            <QuizGradTitle/>
 
-      <Typography color="text.secondary">
-        {t("Welcome!")} <br /> {t("Please create an account.")}
-      </Typography>
+            <Typography color="text.secondary">
+                {t("Welcome!")} <br/> {t("Please create an account.")}
+            </Typography>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FormGroup sx={{ gap: "20px", mt: 4 }}>
-          <FormControl fullWidth>
-            <TextField
-              label={t("Name")}
-              {...register("name", { required: t("Name is required") })}
-              name="name"
-            />
-            <FormHelperText error>{errors.name?.message}</FormHelperText>
-          </FormControl>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <FormGroup sx={{gap: "20px", mt: 4}}>
+                    <FormControl fullWidth>
+                        <TextField
+                            label={t("Name")}
+                            {...register("name", {required: t("Name is required")})}
+                            name="name"
+                        />
+                        <FormHelperText error>{errors.name?.message}</FormHelperText>
+                    </FormControl>
 
-          <FormControl fullWidth>
-            <TextField
-              {...register("password", {
-                required: t("Password is required"),
-                minLength: {
-                  value: 8,
-                  message: t("Password must be at least 8 characters"),
-                },
-              })}
-              name="password"
-              label={t("Password")}
-              type={showPassword.value ? "text" : "password"}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton onClick={showPassword.OnClick}>
-                      {showPassword.value ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <FormHelperText error>{errors.password?.message}</FormHelperText>
-          </FormControl>
+                    <FormControl fullWidth>
+                        <TextField
+                            {...register("password", {
+                                required: t("Password is required"),
+                                minLength: {
+                                    value: 8,
+                                    message: t("Password must be at least 8 characters"),
+                                },
+                            })}
+                            name="password"
+                            label={t("Password")}
+                            type={showPassword.value ? "text" : "password"}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton onClick={showPassword.OnClick}>
+                                            {showPassword.value ? <VisibilityOff/> : <Visibility/>}
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                        <FormHelperText error>{errors.password?.message}</FormHelperText>
+                    </FormControl>
 
-          <FormControl fullWidth>
-            <TextField
-              {...register("confirmPassword", {
-                required: t("Confirm Password is required"),
-                minLength: {
-                  value: 8,
-                  message: t("Password must be at least 8 characters"),
-                },
-              })}
-              name="confirmPassword"
-              label={t("Confirm Password")}
-              type={showConfirmPassword.value ? "text" : "password"}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton onClick={showConfirmPassword.OnClick}>
-                      {showConfirmPassword.value ? (
-                        <VisibilityOff />
-                      ) : (
-                        <Visibility />
-                      )}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <FormHelperText error>
-              {errors.confirmPassword?.message}
-            </FormHelperText>
-          </FormControl>
+                    <FormControl fullWidth>
+                        <TextField
+                            {...register("confirmPassword", {
+                                required: t("Confirm Password is required"),
+                                minLength: {
+                                    value: 8,
+                                    message: t("Password must be at least 8 characters"),
+                                },
+                            })}
+                            name="confirmPassword"
+                            label={t("Confirm Password")}
+                            type={showConfirmPassword.value ? "text" : "password"}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton onClick={showConfirmPassword.OnClick}>
+                                            {showConfirmPassword.value ? (
+                                                <VisibilityOff/>
+                                            ) : (
+                                                <Visibility/>
+                                            )}
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                        <FormHelperText error>
+                            {errors.confirmPassword?.message}
+                        </FormHelperText>
+                    </FormControl>
 
-          <Button variant="useful" type="submit" disabled={loading}>
-            {loading ? t("Loading") : t("Sign up")}
-          </Button>
-        </FormGroup>
-      </form>
-    </Box>
-  );
+                    <FormControl fullWidth>
+                        <ReCAPTCHA
+                            sitekey="6LcCH9EqAAAAAIjMRsfBKIEcAY8G7jNN-pr6gLLb"
+                            onChange={(value: string | null) => setCaptchaValue(value)}
+                        />
+                    </FormControl>
+
+                    <Button variant="useful" type="submit" disabled={loading}>
+                        {loading ? t("Loading") : t("Sign up")}
+                    </Button>
+                </FormGroup>
+            </form>
+        </Box>
+    );
 };
 
 export default SignUpForm;
